76d9a45b1f5db1295b7fd797c983a18501f0058e
Aman Sahu <amansahuking223@gmail.com>
2026-01-29 18:45:24 +0530

    done1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/synapse/",
+  base: "//synapse/tree/Frontend/",
   plugins: [react(),tailwindcss()],
 
 })
